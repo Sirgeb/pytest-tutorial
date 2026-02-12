e801fd4f79b1f9f6d30ca855909d3e383a1c8f15
Gabriel Aniora <chybesta@gmail.com>
2026-02-12 15:20:27 +0100

    pytest marking

--- tests/test_my_functions.py
+++ tests/test_my_functions.py
@@ -1,5 +1,6 @@
 import pytest 
 import source.my_functions as my_functions
+import time
 
 def test_add():
   result = my_functions.add(2, 3)
@@ -17,3 +18,16 @@ def test_divide_by_zero():
   with pytest.raises(ValueError):
     my_functions.divide(10, 0)
 
+@pytest.mark.slow
+def test_very_slow():
+  time.sleep(5)
+  result = my_functions.divide(10, 5)
+  assert result == 2
+
+@pytest.mark.skip(reason="This feature is currently broken")
+def test_add():
+  assert my_functions.add(1, 3) == 3
+
+@pytest.mark.xfail(reason="We know we cannot divide by zero")
+def test_divide_zero_broken():
+  my_functions.divide(4, 0)
